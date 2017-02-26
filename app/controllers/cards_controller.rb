fdef2446442aca3dcd3b1ed594e29e35ecec83e3
Grant <ghwilley@gmail.com>
2017-02-26 16:40:52 -0600

    building create method

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,9 +1,15 @@
 require 'mtg_sdk'
 
 class CardsController < ApplicationController
+	def new
+	end
+
+	def create
+		render plain: params[:card].inspect
+	end
 
 	def show
-  	cards = MTG::Card.where(supertype: 'legendary').all
+  	cards = MTG::Card.where(set: 'ktk').where(subtypes: 'warrior,human').all
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :card
+  resources :cards
 
   root 'main#index'
 
