139f5d93ddc4dbff6ce4e856ee8d91cdd610a189
Grant <ghwilley@gmail.com>
2017-03-04 15:46:31 -0600

    query 80%

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,13 +1,35 @@
 require 'mtg_sdk'
 
 class CardsController < ApplicationController
-  respond_to do |format|
-    format.html
-    format.json { @cards = Card.search_db(params[:term]) }
+  # respond_to do |format|
+  #   format.html
+  #   format.json { @cards = Card.search_db(params[:term]) }
+  # end
+
+  def index  
+# I will explain this part in a moment.
+  if params[:name]
+    @cards = Card.where('name LIKE ?',  "%#{:name}%").order(name)
+  else
+    @cards = Card.all
   end
+
+  respond_to do |format|  
+    format.html # index.html.erb  
+# Here is where you can specify how to handle the request for "/cards.json"
+    format.json { render :json => @cards.to_json }
+    end
+end
 
 
 	def new
+		@card = Card.new
+
+		respond_to do |format|  
+    format.html # index.html.erb  
+# Here is where you can specify how to handle the request for "/cards.json"
+    format.json { render :json => @cards.to_json }
+    end
 	end
 
 	def create
@@ -15,7 +37,7 @@ class CardsController < ApplicationController
 	end
 
 	def show
-  	@cards = MTG::Card.where(set: 'ktk').where(subtypes: 'warrior,human').all
+  	# @cards = MTG::Card.where(set: 'ktk').where(subtypes: 'warrior,human').all
   end
 
 
--- app/views/main/index.json.jbuilder
+++ app/views/main/index.json.jbuilder
@@ -1,7 +1,9 @@
-json.array!(@cards) do |card|
-  json.name        card.name
-  json.multiverse_id       card.multiverse_id
-  json.colors        card.colors
-  json.mana_cost   card.mana_cost
-  json.image_url    card.image_url
-end
+json.extract! @cards, :name
+
+# json.array!(@cards) do |card|
+#   json.name        card.name
+#   json.multiverse_id       card.multiverse_id
+#   json.colors        card.colors
+#   json.mana_cost   card.mana_cost
+#   json.image_url    card.image_url
+# end
